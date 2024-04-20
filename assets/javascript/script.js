5fe44df24822cadbf14732fe5c4c899cb19c39f8
Ian Garrigan <igarrigan@hotmail.com>
2024-04-20 05:55:08 +0100

    Customize owl with jquery for bestsellers slide

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ function contactForm()
     event.preventDefault;
     firstName = document.getElementById("firstName").value;
     if (firstName == "")
-    {
+        {
         alert("Please enter a first name and email address so we can best respond to your query. Thank you.");
     }
     else 
@@ -13,3 +13,46 @@ function contactForm()
     }
     
 }
+
+// customized owl carousel functionality IAN.G
+$(document).ready(function() {
+
+
+    //Init the carousel
+    $("#bestsellerSlider").owlCarousel({
+      items: 4,
+      center: true,
+      nav: true,
+      lazyLoad: true,
+    //   fontawesome html injection
+      navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+      autoplay: true,
+      autoplayTimeout:4000,
+      onInitialized: createProgressBar,
+      onTranslate: resetProgressBar,
+      onTranslated: createProgressBar
+    });
+
+     function createProgressBar() {
+      // apply keyframe animation
+      $("#bar").css({
+        transition: "width 4000ms",
+        width: "100%"
+      });
+    }
+
+    function resetProgressBar() {
+      $("#bar").css({
+        transition: "width 0s",
+        width: 0
+      });
+    }
+    //Init progressBar where elem is $("#bestsellerSlider")
+    function progressBar(elem){
+      $elem = elem;
+      //build progress bar elements
+      createProgressBar();
+      //start counting
+      start();
+    }
+});
